9625dce89903c54aa33d8333364b77670e57d02e
zmateusz <mat309@o2.pl>
2015-09-28 23:22:48 +0200

    Data load test (it works)

--- my_script.js
+++ my_script.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-  $('#main').append("Music Mug");
+  var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1geo9Y7-A-vClJ7dCsNH0ZydgRPE_6Mi5jG24Lg6_0nI/edit?usp=sharing#gid=0';
+  
+  $('#statistics').sheetrock({
+    url: mySpreadsheet
+  });  
 });
